75d9de2ed5822cb2da87612c95f600de2ff72d12
Nistha Gautam <nish271996@gmail.com>
2019-09-24 22:05:10 -0400

    Update schema-patient.sql

--- app/data/schema-patient.sql
+++ app/data/schema-patient.sql
@@ -9,8 +9,8 @@ CREATE TABLE Patient (
     sexAtBirth CHAR(1) DEFAULT ''
 );
 
-INSERT INTO Patient (patientGuid, firstName, lastName, sexAtBirth, dob) VALUES
-("SOME-REALLY-LONG-1234", "Sylvia", "Hernandez",  "F", "2012-09-01"),
-("SOME-REALLY-SHORT-5678", "Vish", "Balasubramanian", "M", "1950-12-15"),
-("SOME-UNIQUE-ABCDE1", "John", "Doe", "","1950"),
-("SOME-DUMMY-DATA", "Pepper", "Potts",  "F", "1990-01-31");
+INSERT INTO Patient (patientGuid, firstName, lastName, dob, sexAtBirth) VALUES
+("SOME-REALLY-LONG-1234", "Sylvia", "Hernandez", "2012-09-01",  "F"),
+("SOME-REALLY-SHORT-5678", "Vish", "Balasubramanian", "1950-12-15", "M"),
+("SOME-UNIQUE-ABCDE1", "John", "Doe","1950", "M"),
+("SOME-DUMMY-DATA", "Pepper", "Potts", "1990-01-31",  "F");
