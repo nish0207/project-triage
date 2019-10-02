f2d68bf51eb8f77ba86c8e15ea19180d4161ad9d
Nistha Gautam <nish271996@gmail.com>
2019-10-02 11:22:55 -0400

    post data to db

--- app/public/js/triage.app.js
+++ app/public/js/triage.app.js
@@ -8,11 +8,14 @@ var patientTriageApp = new Vue({
       // TODO: Add the correct date via Javascript before posting
 
        // TODO:
-       // fetch(url, {
-       //   method:'post',
-       //   data: this.patient
-       // })
-       // .then( ... )
+        fetch(url, {
+          method:'POST',
+          body: JSON.stringify(this.patient),
+          headers: {
+            "Content-Type": "application/json; charset= utf-8"
+          }
+        })
+        .then( ... )
        waitingApp.patients.push(this.patient);
        this.handleReset();
     },
