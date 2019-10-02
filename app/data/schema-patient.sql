cadac794c20f926494c65e95b7fe87d9f730e000
Nistha Gautam <nish271996@gmail.com>
2019-10-02 10:41:44 -0400

    sql queries added

--- app/data/schema-patient.sql
+++ app/data/schema-patient.sql
@@ -14,3 +14,15 @@ INSERT INTO Patient (patientGuid, firstName, lastName, dob, sexAtBirth) VALUES
 ("SOME-REALLY-SHORT-5678", "Vish", "Balasubramanian", "1950-12-15", "M"),
 ("SOME-UNIQUE-ABCDE1", "John", "Doe","1950", "M"),
 ("SOME-DUMMY-DATA", "Pepper", "Potts", "1990-01-31",  "F");
+
+
+CREATE TABLE PatientVisit (
+  visitID INTEGER PRIMARY KEY AUTO_INCREMENT,
+  patientGuid VARCHAR(64) UNIQUE,
+  visitDescription TEXT NOT NULL DEFAULT '',
+  visitDateUtc DATETIME DEFAULT CURRENT_TIMESTAMP,
+  priority ENUM('Low','Medium','High') NOT NULL DEFAULT 'Low',
+  FOREIGN KEY patientGuid REFERENCES Patient(patientGuid)
+);
+
+INSERT INTO PatientVisit(patientGuid, visitDescription, visitDateUtc, priority)
